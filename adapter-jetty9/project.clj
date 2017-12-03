3cacba59d575aa349d16876318d0e604df60de00
Pieter van Prooijen <pprooi@xs4all.nl>
2017-12-03 12:47:17 +0100

    Update to latest leiningen and jetty-9 versions

--- adapter-jetty9/project.clj
+++ adapter-jetty9/project.clj
@@ -1,11 +1,13 @@
+(def jetty-version "9.4.8.v20171121")
+
 (defproject lein-servlet/adapter-jetty9 "0.4.1"
   :description "Jetty 9 adapter for lein-servlet"
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[org.eclipse.jetty/jetty-server  "9.2.6.v20141205"]
-                 [org.eclipse.jetty/jetty-servlet "9.2.6.v20141205"]
-                 [org.eclipse.jetty/jetty-webapp  "9.2.6.v20141205"]
-                 [org.eclipse.jetty/jetty-jsp     "9.2.6.v20141205"]
-                 [org.eclipse.jetty/jetty-jndi    "9.2.6.v20141205"]])
+  :dependencies [[org.eclipse.jetty/jetty-server  ~jetty-version]
+                 [org.eclipse.jetty/jetty-servlet ~jetty-version]
+                 [org.eclipse.jetty/jetty-webapp  ~jetty-version]
+                 [org.eclipse.jetty/jetty-http    ~jetty-version]
+                 [org.eclipse.jetty/jetty-jndi    ~jetty-version]])
--- plugin/project.clj
+++ plugin/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/kumarshantanu/lein-servlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.8.1"
   :dependencies []
   :warn-on-reflection true
   :eval-in :leiningen
